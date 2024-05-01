b8cec0dd9a831a3027e12beea7463cf7ceaf1cc8
AnnaKuliavets <51449539+AnnaKuliavets@users.noreply.github.com>
2024-05-01 19:45:25 +0200

    HW12.py

--- HW12.py
+++ HW12.py
@@ -5,7 +5,7 @@ words = sentences.split()
 result_words = []
 
 for word in words:
-    if 'o' in word.lower():
+    if 'oo' in word.lower():
         result_words.append(word.capitalize())
 
 print(Fore.BLUE + "Results:", ' '.join(result_words))
