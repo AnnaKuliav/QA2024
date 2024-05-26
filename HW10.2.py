491ed64b27b53185c531a1f4af8f5ffa3833e2e7
AnnaKuliavets <51449539+AnnaKuliavets@users.noreply.github.com>
2024-05-26 16:48:52 +0200

    HW10.2.py

--- HW10.2.py
+++ HW10.2.py
@@ -1,13 +1,18 @@
-word = input('put word for checking ')
-symbol = [' ']
-for x in symbol:
-    word = word.replace(x,'')
-    word = word.lower()
-s = str(word)
-l = len(s)
-for i in range(l//2):
-    if s[i] != s[-1-i]:
-       print("True")
-       break
-else:
-    print("False")
+def is_palindrome_method2(s):
+    s = s.strip().lower()
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+        left += 1
+        right -= 1
+
+    return True
+
+
+# Examples
+print(is_palindrome_method2("    aBC cba "))  # True
+print(is_palindrome_method2("a BCQcb a    "))  # True
+print(is_palindrome_method2(" ab bca"))  # False
+print(is_palindrome_method2("ab b a"))  # False
